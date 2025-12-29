30653e496c632f49dcc4c3d18f89ba6ec8431d9d
Elthecreator7 <elthecreator7@gmail.com>
2025-12-29 09:57:43 +0000

    Added optimized solution without append

--- chapter8_slices/lesson7/main.go
+++ chapter8_slices/lesson7/main.go
@@ -11,3 +11,16 @@ func createMatrix(rows, cols int) [][]int {
 	}
 	return matrix
 }
+
+// SOLUTION WITHOUT APPEND(OPTIMIZED BUT LESS READABLE)
+
+// func createMatrix(rows, cols int) [][]int {
+// 	matrix := make([][]int, rows)
+// 	for i := 0; i < rows; i++ {
+// 		matrix[i] = make([]int, cols)
+// 		for j := 0; j < cols; j++ {
+// 			matrix[i][j] = i * j
+// 		}
+// 	}
+// 	return matrix
+// }
