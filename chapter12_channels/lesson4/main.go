e5116a9f211ede71dac96558df2facf188042b0e
Elthecreator7 <elthecreator7@gmail.com>
2026-01-13 20:58:41 +0000

    Buffered channels assignment solution

---
+++ chapter12_channels/lesson4/main.go
@@ -0,0 +1,9 @@
+package main
+
+func addEmailsToQueue(emails []string) chan string {
+	ch := make(chan string, len(emails))
+	for i := 0; i < len(emails); i++ {
+		ch <- emails[i]
+	}
+	return ch
+}
